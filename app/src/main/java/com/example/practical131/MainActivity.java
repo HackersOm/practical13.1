0857b3c45e1df908ee8ac5fea961b7059c9f03ab
lenovo <hackeromsutar@gmail.com>
2024-01-19 15:44:44 +0530

    Initial commit

---
+++ app/src/main/java/com/example/practical131/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.practical131;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+    int i=0;
+    int j=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
